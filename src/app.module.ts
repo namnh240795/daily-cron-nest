e918eeb5c258d21d74d8e12f02ca99f1850f7012
Nam Nguyen <nam.nh@agiletech.vn>
2022-01-07 17:27:50 +0700

    Update cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(AppService.name);
+
+  @Cron('0 55 17 * * 1-6')
+  async handleReport() {
+    const data = {
+      content: 'Report nhé mọi người @everyone',
+    };
+
+    axios.post(
+      'https://discord.com/api/v8/webhooks/928930500125540372/F8jAOCclkP_pojrN-BJLVrNIHO8IZaPV9biYjccvcfHUoh3aWSYNEZL6YV7sOx7fq9Re',
+      data,
+    );
+  }
+
+  @Cron('0 0 9 * * 1-6')
+  async handleMeeting() {
+    const data = {
+      content: 'Vào họp nào mọi người',
+      embeds: [
+        {
+          title: 'Daily meeting',
+          description: 'Google link \n https://meet.google.com/ghm-hrqj-bov',
+          color: 5814783,
+        },
+      ],
+    };
+
+    axios.post(
+      'https://discord.com/api/v8/webhooks/928930500125540372/F8jAOCclkP_pojrN-BJLVrNIHO8IZaPV9biYjccvcfHUoh3aWSYNEZL6YV7sOx7fq9Re',
+      data,
+    );
   }
 }
